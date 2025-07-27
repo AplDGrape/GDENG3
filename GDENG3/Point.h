ce947f6f8799c7770c07d2b2c2233b91b93bdb82
AplDGrape <61457409+AplDGrape@users.noreply.github.com>
2025-07-27 15:56:13 +0800

    Update Point.h

--- GDENG3/Point.h
+++ GDENG3/Point.h
@@ -5,18 +5,15 @@ class Point
 public:
 	Point() : m_x(0), m_y(0)
 	{
-		/*this->x = 0;
-		this->y = 0;*/
+
 	}
 	Point(int x, int y) :m_x(x), m_y(y)
 	{
-		/*this->x = x;
-		this->y = y;*/
+
 	}
 	Point(const Point& point) : m_x(point.m_x), m_y(point.m_y)
 	{
-		/*this->x = point.x;
-		this->y = point.y;*/
+
 	}
 	~Point()
 	{
